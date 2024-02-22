2f0eca59fc22ae76d057b12f93532f06a89b046b
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-02-22 14:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const student = {
-  name: "Rahul",
+const myObj = {
+  name: "John",
+  age: 30,
+  city: "New York",
 };
 
 Object.prototype.getKeys = function () {
   return Object.keys(this);
 };
 
+const keys = myObj.getKeys();
+console.log(keys);
